ec7625ee10ac7a8e72002c7800593306dfd0245e
jo-lyn <e0007247@u.nus.edu>
2018-03-24 23:18:16 +0800

    convert indentation to spaces

--- components/toggler.js
+++ components/toggler.js
@@ -1,26 +1,26 @@
 AFRAME.registerComponent('toggler', {
-	schema: {
-		isVisible: {default: [true, true, true, true, true, true, true, true]},
-		toggled: {default: '-1'}
-	},
+    schema: {
+        isVisible: {default: [true, true, true, true, true, true, true, true]},
+        toggled: {default: '-1'}
+    },
 
-	toggle: function(value) {
-		var HIDDEN_POSITION = {'x': 10000, 'y': 10000, 'z': 10000};
+    toggle: function(value) {
+        var HIDDEN_POSITION = {'x': 10000, 'y': 10000, 'z': 10000};
 
-		// update toggled values
-		this.data.isVisible[value-1] = !this.data.isVisible[value-1];
-		this.data.toggled = value;
+        // update toggled values
+        this.data.isVisible[value-1] = !this.data.isVisible[value-1];
+        this.data.toggled = value;
 
-  		// update vertices
-  		for (i = 0; i < vertices.length; i++) {
-  			if (json[i].demog === value) {
-  				if (this.data.isVisible[value-1]) {
-  					geometry.vertices[i] = vertices[i];
-  				} else {
-  					geometry.vertices[i] = HIDDEN_POSITION;
-  				}
-  			}
-  		}
-  		geometry.verticesNeedUpdate = true;
-  	}
+        // update vertices
+        for (i = 0; i < vertices.length; i++) {
+            if (json[i].demog === value) {
+                if (this.data.isVisible[value-1]) {
+                    geometry.vertices[i] = vertices[i];
+                } else {
+                    geometry.vertices[i] = HIDDEN_POSITION;
+                }
+            }
+        }
+        geometry.verticesNeedUpdate = true;
+    }
 });
--- index.js
+++ index.js
@@ -4,23 +4,23 @@ var json, geometry;
 var vertices = [];
 
 $(document).ready(function () {
-	init();
+    init();
 });
 
 function init() {
-	plot = document.querySelector('a-scatterplot');
-	geometry = plot.object3D.children[0].geometry;
+    plot = document.querySelector('a-scatterplot');
+    geometry = plot.object3D.children[0].geometry;
 
-	for (i = 0; i < geometry.vertices.length; i++) {
-		vertices[i] = geometry.vertices[i];
-	}
+    for (i = 0; i < geometry.vertices.length; i++) {
+        vertices[i] = geometry.vertices[i];
+    }
 
-	$.getJSON(FILE_PATH, function(result) {
-		json = result;
-	});
+    $.getJSON(FILE_PATH, function(result) {
+        json = result;
+    });
 }
 
 function toggle(value) {
-	var toggler = plot.components.toggler;
-	toggler.toggle(value);
+    var toggler = plot.components.toggler;
+    toggler.toggle(value);
 }
