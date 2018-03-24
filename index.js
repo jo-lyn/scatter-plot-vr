9613bd4503d6763eba7b52cb4a65f2f7b1758632
jo-lyn <e0007247@u.nus.edu>
2018-03-24 23:05:52 +0800

    add toggler component

---
+++ index.js
@@ -0,0 +1,26 @@
+var FILE_PATH = 'datasets/sample.json';
+var plot;
+var json, geometry;
+var vertices = [];
+
+$(document).ready(function () {
+	init();
+});
+
+function init() {
+	plot = document.querySelector('a-scatterplot');
+	geometry = plot.object3D.children[0].geometry;
+
+	for (i = 0; i < geometry.vertices.length; i++) {
+		vertices[i] = geometry.vertices[i];
+	}
+
+	$.getJSON(FILE_PATH, function(result) {
+		json = result;
+	});
+}
+
+function toggle(value) {
+	var toggler = plot.components.toggler;
+	toggler.toggle(value);
+}
