89e586379328b530c5ee6630b5c743d6a375d1ad
jo-lyn <e0007247@u.nus.edu>
2018-03-28 20:10:12 +0800

    fix undefine attributes

--- index.js
+++ index.js
@@ -9,16 +9,29 @@ $(document).ready(function () {
 
 function init() {
     plot = document.querySelector('a-scatterplot');
-    geometry = plot.object3D.children[0].geometry;
 
-    for (i = 0; i < geometry.vertices.length; i++) {
-        vertices[i] = geometry.vertices[i];
+    try {
+        geometry = plot.object3D.children[0].geometry;
+
+        for (i = 0; i < geometry.vertices.length; i++) {
+            vertices[i] = geometry.vertices[i];
+        }
+
+        $.getJSON(FILE_PATH, function(result) {
+            json = result;
+        });
+
+        if (geometry == "" || json == "") {
+            throw "data attributes undefined";
+        }
     }
-
-    $.getJSON(FILE_PATH, function(result) {
-        json = result;
-    });
-}
+    catch (err) {
+        // failed to load data from plot, retry after 1s
+        setTimeout(function () {
+            init();
+        }, 1000);
+    }
+} 
 
 function toggle(value) {
     var toggler = plot.components.toggler;
