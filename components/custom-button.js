da523be672ee4af06679a5cdfcafd55f513b30d8
jo-lyn <e0007247@u.nus.edu>
2018-04-06 16:08:24 +0800

    add toggle functionality to buttons

--- components/custom-button.js
+++ components/custom-button.js
@@ -6,6 +6,10 @@
  * Updates its position to move along the controller.
  */
 AFRAME.registerComponent('custom-button', {
+    schema: {
+        isVisible: {default: [true, true, true, true, true, true, true, true]}
+    },
+
     init: function () {
         this.ACTIVE_STATE = 'active';
         this.onButtondown = this.onButtondown.bind(this);
@@ -28,28 +32,36 @@ AFRAME.registerComponent('custom-button', {
         // discrete action handler
         switch (this.el.getAttribute('id')) {
             case "lgd1":
-                // do something for lgd1
+                this.toggle(1);
+                document.querySelector("#msg").setAttribute("value", ".> Male Malays");
                 break;
             case "lgd2":
-                // do something for lgd2
+                this.toggle(2);
+                document.querySelector("#msg").setAttribute("value", ".> Female Malays");
                 break;
             case "lgd3":
-                // do something for lgd3
+                this.toggle(3);
+                document.querySelector("#msg").setAttribute("value", ".> Male Chinese");
                 break;
             case "lgd4":
-                // do something for lgd4
+                this.toggle(4);
+                document.querySelector("#msg").setAttribute("value", ".> Female Chinese");
                 break;
             case "lgd5":
-                // do something for lgd5
+                this.toggle(5);
+                document.querySelector("#msg").setAttribute("value", ".> Male Indians");
                 break;
             case "lgd6":
-                // do something for lgd6
+                this.toggle(6);
+                document.querySelector("#msg").setAttribute("value", ".> Female Indians");
                 break;
             case "lgd7":
-                // do something for lgd7
+                this.toggle(7);
+                document.querySelector("#msg").setAttribute("value", ".> Male Others");
                 break;
             case "lgd8":
-                // do something for lgd8
+                this.toggle(8);
+                document.querySelector("#msg").setAttribute("value", ".> Female Others");
                 break;
         }
     },
@@ -88,5 +100,28 @@ AFRAME.registerComponent('custom-button', {
                     break;
             }
         }
+    },
+
+    /**
+     * Toggles visibility of points with the given demographic value.
+     * @param {number} value - demographic value of the range [1,8].
+     */
+    toggle: function(value) {
+        var HIDDEN_POSITION = {'x': 10000, 'y': 10000, 'z': 10000};
+
+        // update visibility
+        this.data.isVisible[value - 1] = !this.data.isVisible[value - 1];
+
+        // update vertices
+        for (i = 0; i < vertices.length; i++) {
+            if (json[i].demog === value) {
+                if (this.data.isVisible[value - 1]) {
+                    geometry.vertices[i] = vertices[i];
+                } else {
+                    geometry.vertices[i] = HIDDEN_POSITION;
+                }
+            }
+        }
+        geometry.verticesNeedUpdate = true;
     }
 });
